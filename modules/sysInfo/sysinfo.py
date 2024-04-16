83d4a4be857b1011ad438b78945aa00b0c6dee97
Davipcrs <davi.coelho_comp@outlook.com>
2024-04-16 01:07:14 -0300

    Project Structure change

--- modules/sysInfo/sysinfo.py
+++ modules/sysInfo/sysinfo.py
@@ -1,1 +1,3 @@
 # https://learn.microsoft.com/en-us/windows/win32/wmisdk/wmi-tasks-for-scripts-and-applications
+# https://medium.com/@marcovit87/pywin32-and-wmi-windows-mangement-instrumentation-navigating-windows-os-with-python-4a73bfcce59a#:~:text=Windows%20Management%20Instrumentation%20(WMI)%20is,of%20the%20Windows%20API%20functions.
+import wmi
