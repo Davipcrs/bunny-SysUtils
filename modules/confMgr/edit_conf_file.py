791a3103c4a7e218f7d6918f3546437218001d15
Davi Coelho <davi.coelho_comp@outlook.com>
2024-05-22 21:14:59 -0300

    Create addBackupFolderAndOutput function

--- modules/confMgr/edit_conf_file.py
+++ modules/confMgr/edit_conf_file.py
@@ -1,1 +1,20 @@
 # Needs implementation.
+from modules.confMgr.load_conf_file import loadConfFile
+import json
+
+def addBackupFolderAndOutput(folder: str, output: str):
+    conf = loadConfFile()
+    conf["backup_conf"]["folders"].append(folder)
+    conf["backup_conf"]["outputs"].append(output)
+
+    with open("config.json", "w") as config:
+        json_obj = json.dumps(conf, indent=4)
+        config.write(json_obj)
+
+        config.close()
+    
+    return
+
+def removeBackupFolderAndOutput(index: int):
+    conf = loadConfFile()
+    return
